c37a584dfb45aa3ef7ec25b27afb5a2f4e7be8c7
Martin Renteria <martinrenteria@Martins-MBP.lan>
2023-10-04 21:46:37 -0600

    refactored code more

--- Develop/script.js
+++ Develop/script.js
@@ -12,13 +12,13 @@ generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
-  var correctPrompts = windowPrompts();
+  var window = windowPrompts();
   var passwordText = document.querySelector("#password")
-  if(correctPrompts) {
-  var newPassword = generatePassword();
-  passwordText.value = newPassword;
+  if(window) {
+  var Password = generatePassword();
+  passwordText.value = Password;
   } else {
-    passwordText.value = "";
+    passwordText.value = '';
   }
 }
 
@@ -26,47 +26,46 @@ function writePassword() {
 var characterLength = 8;
 function generatePassword() {
  
-  var password = "";
-  for(var i = 0; i < characterLength; i++) {
-    var randomIndex = Math.floor(Math.random() * choiceArray.length); 
-    password = password + choiceArray[randomIndex]; 
+  var password = '';
+  for(var i = 0; i < characterLength; ++i) {
+    var randomIndex = Math.floor(Math.random() * choices.length); 
+    password = password + choices[randomIndex]; 
   }
   return password;
 }
  
-var choiceArray = [];  
-var upperCaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
+var choices = [];  
+var uppers = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ];
-var numberArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',];
-var lowerCaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',];
+var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 
 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var specialCharacterArray = ['!', 'S', '&', '^', '?', '+' ];
+var specialchar = ['!', 'S', '&', '^', '?', '+' ];
  
 
 
 function windowPrompts() {
-  choiceArray = [];
-  characterLength = parseInt(prompt(" How many characters would you like? (8 - 128 characters"));
+  choices = [];
+  characterLength = parseInt(prompt(' How many characters would you like? (8 - 128 characters'));
   if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
-    alert("Character length has to be a number between 8 and 128");
+    alert('Character length must be between 8 and 128');
     return false
   }
   
-  if (confirm("would you like numbers in your password?")) {
-    choiceArray = choiceArray.concat(numberArray);
+  if (confirm(' Would you like numbers in your password?')) {
+    choices = choices.concat(numbers);
   }
-   if(confirm("would you like upppercase letters in your password?")) {
-    choiceArray = choiceArray.concat(upperCaseArray);
+   if(confirm(' Would you like upppercase letters in your password?')) {
+    choices = choices.concat(uppers);
   }
- if (confirm("would you like lowercase letters in your password?")) {
-    choiceArray = choiceArray.concat(lowerCaseArray);
-  } 
- if (confirm("would you like special characters in your password?")) {
-    choiceArray = choiceArray.concat(specialCharacterArray);
+ if (confirm(' Would you like lowercase letters in your password?')) {
+    choices = choices.concat(lowercase);
+ if (confirm(' Would you like special characters in your password?')) {
+    choices = choices.concat(specialchar);
   } 
   return true
   
-  
+}
 }
 
   // return "Generate password will go here!";
@@ -88,4 +87,4 @@ function windowPrompts() {
 // 
 
 // function windowprompts(){
-  // characterLength = parseInt
+// characterLength = parseIn
